7e6960126d82f757507c1674a444af33c4b81c36
Ariella <aya.navarro@me.com>
2020-08-12 15:44:00 -0700

    edit path

--- server/index.js
+++ server/index.js
@@ -44,11 +44,11 @@ if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
-  // // Express serve up index.html file if it doesn't recognize route
-  // const path = require('path');
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  // });
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
 
   app.get('/login', function(req, res) {
     res.redirect('https://accounts.spotify.com/authorize?' +
