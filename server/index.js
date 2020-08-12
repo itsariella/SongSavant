7341b3aef01ce7da184ed02a1d78497a82f87088
Ariella <aya.navarro@me.com>
2020-08-12 15:37:25 -0700

    edit path

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.js'));
   });
 
   app.get('/login', function(req, res) {
