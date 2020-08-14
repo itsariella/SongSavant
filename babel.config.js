cfc31f5196ba4e00bcdc21735139c8377c9e463f
Ariella <aya.navarro@me.com>
2020-08-13 22:02:40 -0700

    hide config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ],
-  plugins: process.env.NODE_ENV==='production' ? ["transform-remove-console"] : []
-}
+module.exports = function override(api) {
+
+    var env = api.cache(() => process.env.NODE_ENV);
+    var isProd = api.cache(() => process.env.NODE_ENV === "production");
+  
+    if (!isProd) {
+      config = {
+        plugins: [
+          ["transform-remove-console"]
+        ],
+        presets: ["@babel/preset-flow", "module:metro-react-native-babel-preset"]
+      };
+    }
+  
+    return config;
+  };
